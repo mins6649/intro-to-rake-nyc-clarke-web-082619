3bbe325db066ef8bb39a3e0416b66a7981f62ff7
Minyoung Shin <mins6649@gmail.com>
2021-06-08 18:02:33 -0400

    hm

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 require_relative "../lib/student.rb"
 
 Student.create(name: "Melissa", grade: "10th")
@@ -5,3 +6,6 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+
+
